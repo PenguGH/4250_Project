ce5bc49c8974b8560e0d83e92c39a6fbb98a0071
OJ101003 <hackerdude1010@gmail.com>
2025-04-18 20:03:32 -0700

    Finished Vector Space Model

--- improved_retrieval/retrieval.py
+++ improved_retrieval/retrieval.py
@@ -1,14 +1,7 @@
 from nltk.stem import SnowballStemmer
-import os
 import math
 import csv
-import sys
-
-
-def safe_print(data):
-    encoded = str(data).encode('ascii', errors='replace').decode('ascii')
-    print(encoded)
-
+from collections import defaultdict
 
 def stemmerFunc(wordList):
     stemmer = SnowballStemmer("english")
@@ -17,55 +10,75 @@ def stemmerFunc(wordList):
         returnList.append(stemmer.stem(word))
     return returnList
 
-def main():
-    inverted_index = {}
-    with open('inverted_index_single_line.csv', 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        counter = 10
-        for row in reader:
-            inverted_index[row[0]] = [doc_freq.split(":") for doc_freq in row[1:]]
-            if counter >= 0:
-
-                # safe_print(row[0])
-                # # safe_print(row[1])
-                # safe_print(inverted_index[row[0]])
-
-                counter -= 1
-    # Pretty output to file
-    # For debugging
-    # with open('inverted_index_output.txt', 'w', encoding='utf-8') as f:
-    #     for term, postings in inverted_index.items():
-    #         f.write(f"{term}: {postings}\n")
-
-    # query = input("Enter query: ")
-    query = "Los angeles dodgers"
-    query = query.lower()
-    query = query.split(" ")
-    query = stemmerFunc(query)
-
-    # # For debugging
-    # print(query)
-
-    df = { # How many documents each term appears in
-        term: len(postings) for term, postings in inverted_index.items()
-    }
-    # safe_print(df)
-
-    N = 500 # Number of documents in tokenized_project2_project2_crawl
-    idf = {term: math.log(N / df_val) for term, df_val in df.items()}
-    # safe_print(idf)
-
+def queryVector(query, inverted_index, idf):
     query_tf = {term: query.count(term) for term in query}
-    query_vector = []
+    query_vector = [] # This is the TF-IDF of each term in the query
     for term in query:
-        # print(term, query_tf[term], idf[term])
         if term in inverted_index:
             query_vector.append(query_tf[term] * idf[term])
         else:
             query_vector.append(0)
+    return query_vector
 
-    # safe_print(query_vector)
-    # safe_print(query)
+
+def documentVectors(inverted_index, idf, query_terms):
+    docVector = defaultdict(lambda: [0.0] * len(query_terms))
+    termIndex = {term: i for i, term in enumerate(query_terms)}
+
+    for term in query_terms:
+        if term in inverted_index:
+            postings = inverted_index[term]
+            for doc_id, tf in postings:
+                tf_idf = int(tf) * idf[term]
+                index = termIndex[term]
+                docVector[doc_id][index] = tf_idf
+    return docVector
+
+def cosine_similarity(queryVector, docVector):
+    cosineVector = []
+    for index, value in enumerate(docVector):
+        dot = sum(queryVector[i] * docVector[value][i] for i in range(len(queryVector)))
+        magQ = math.sqrt(sum(queryVector[i] ** 2 for i in range(len(queryVector))))
+        magD = math.sqrt(sum(docVector[value][i] ** 2 for i in range(len(docVector[value]))))
+        if magQ == 0 or magD == 0:
+            cosine = 0
+        else:
+            cosine = dot / (magQ * magD)
+        cosineVector.append([value, cosine])
+    cosineVector.sort(key=lambda x: x[1], reverse=True)
+    return cosineVector
+
+def main():
+    inverted_index = {}
+    with open('inverted_index_single_line.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            inverted_index[row[0]] = [doc_freq.split(":") for doc_freq in row[1:]]
+
+    query = input("Enter query: ")
+    query = query.lower()
+    query = query.split(" ")
+    query = stemmerFunc(query)
+
+    df = { # How many documents each term appears in
+        term: len(postings) for term, postings in inverted_index.items()
+    }
+    N = 500 # Number of documents in tokenized_project2_project2_crawl
+    idf = {term: math.log(N / df_val) for term, df_val in df.items()}
+
+    query_vector = queryVector(query, inverted_index, idf)
+    docVectors = documentVectors(inverted_index, idf, query) # Returns document vectors in the same order as the original search query
+    cosineVector = cosine_similarity(query_vector, docVectors)
+
+    numToPrint = input("Up to how many pages do you want to print, type 0 for all: ")
+    if numToPrint == "0" or int(numToPrint) > len(cosineVector):
+        numToPrint = len(cosineVector)
+    else:
+        numToPrint = int(numToPrint)
+
+    for index, doc in enumerate(cosineVector[:numToPrint]):
+        print(f'Document Name: {doc[0].split(".")[0]}, Cosine Similarity: {doc[1]}, Rank: {index + 1}')
+    
 
 if __name__ == "__main__":
     main()
